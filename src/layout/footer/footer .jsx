e5f357684bf0d25dba38ab885b74f1f645a8c092
Anshul <anshul007saxena@gmail.com>
2025-07-21 11:21:52 +0530

    changes in layout

---
+++ src/layout/footer/footer .jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Box, Typography, Container, Grid } from '@mui/material';
+
+const drawerWidth = 240;
+const miniDrawerWidth = 70;
+
+const Footer = ({ isSidebarExpanded }) => {
+  return (
+    <Box
+      component="footer"
+      sx={{
+        backgroundColor: '#2c3e50',
+        color: '#ecf0f1',
+        py: 3,
+        mt: 'auto',
+        transition: 'margin-left 0.3s ease-in-out',
+        marginLeft: isSidebarExpanded ? `${drawerWidth}px` : `${miniDrawerWidth}px`,
+         zIndex: 1200,
+      }}
+    >
+      <Container maxWidth="lg">
+        <Grid container spacing={1} justifyContent="space-between" alignItems="center">
+          <Grid
+            item
+            xs={12}
+            md={4}
+            sx={{
+                ml: isSidebarExpanded ? `${0}px` : `${0}px`, 
+                transition: 'margin-left 0.3s ease-in-out',
+            }}
+            >
+            <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+                STPI
+            </Typography>
+            <Typography variant="body2">
+                Software Technology Parks of India
+            </Typography>
+        </Grid>
+          <Grid item xs={12} md={4} sx={{ textAlign: { xs: 'left', md: 'right' } }}>
+            <Typography variant="body2">
+              © {new Date().getFullYear()} STPI. All rights reserved.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
--- src/pages/login/loginPannel/LoginPanel.jsx
+++ src/pages/login/loginPannel/LoginPanel.jsx
@@ -27,8 +27,10 @@ const LoginPanel = () =>{
         const response = await postLogin(payload)
         if(response?.data?.statusCode === 200){
             const userRole = response?.data?.user?.role;
+            const userName = response?.data?.user?.name;
             localStorage.setItem('isAuthenticated', 'true');
             localStorage.setItem('userRole', userRole);
+            localStorage.setItem('userName', userName);
              Swal.fire({
                 icon: 'success',
                 title: 'Login Successful',
@@ -55,60 +57,112 @@ const LoginPanel = () =>{
                 minHeight: '100vh',
                 background: 'linear-gradient(to right, #283E51, #485563)',
                 display: 'flex',
+                flexDirection: 'column',
+            }}
+        >   
+            <Box
+                sx={{
+                    height: 64,
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    px: 4,
+                    backgroundColor: '#2c3e50',
+                    color: '#ecf0f1',
+                }}
+            >
+                <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+                    PMC Portal
+                </Typography>
+                <Typography variant="subtitle1">STPI</Typography>
+            </Box>
+            <Box
+                sx={{
+                flexGrow: 1,
+                display: 'flex',
                 alignItems: 'center',
                 justifyContent: 'center',
-                p: 2
-            }}
-        >
-        <Container maxWidth="sm">
-            <Paper elevation={6} sx={{p: 4, borderRadius: 3,backgroundColor: '#fdfdfd',boxShadow: '0 8px 24px rgba(0,0,0,0.1)',animation: 'fadeIn 0.6s ease-in-out',}}>
-                <Typography variant="h5" align="center" gutterBottom  sx={{ fontWeight: 'bold', color: '#1976d2' }}>PMC Login</Typography>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <TextField
-                        fullWidth
-                        label="Username"
-                        variant="outlined"
-                        margin="normal"
-                        {...register('username')}
-                        error={!!errors.username}
-                        helperText={errors.username?.message}
-                        autoComplete="username"
-                    />
-                    <TextField
-                        fullWidth
-                        label="Password"
-                        variant="outlined"
-                        margin="normal"
-                        type={showPassword ? 'text' : 'password'}
-                        {...register('password')}
-                        error={!!errors.password}
-                        helperText={errors.password?.message}
-                        autoComplete="current-password"
-                        InputProps={{
+                p: 2,
+                }}
+            >
+                <Container maxWidth="sm">
+                    <Paper
+                        elevation={6}
+                        sx={{
+                        p: 4,
+                        borderRadius: 3,
+                        backgroundColor: '#fdfdfd',
+                        boxShadow: '0 8px 24px rgba(0,0,0,0.1)',
+                        animation: 'fadeIn 0.6s ease-in-out',
+                        }}
+                    >
+                    <Typography
+                    variant="h5"
+                    align="center"
+                    gutterBottom
+                    sx={{ fontWeight: 'bold', color: '#1976d2' }}
+                    >
+                    PMC Login
+                    </Typography>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <TextField
+                            fullWidth
+                            label="Username"
+                            variant="outlined"
+                            margin="normal"
+                            {...register('username')}
+                            error={!!errors.username}
+                            helperText={errors.username?.message}
+                            autoComplete="username"
+                        />
+                        <TextField
+                            fullWidth
+                            label="Password"
+                            variant="outlined"
+                            margin="normal"
+                            type={showPassword ? 'text' : 'password'}
+                            {...register('password')}
+                            error={!!errors.password}
+                            helperText={errors.password?.message}
+                            autoComplete="current-password"
+                            InputProps={{
                             endAdornment: (
                                 <InputAdornment position="end">
-                                    <IconButton onClick={togglePassword} edge="end">
-                                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                                    </IconButton>
+                                <IconButton onClick={togglePassword} edge="end">
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
                                 </InputAdornment>
                             ),
-                        }}
-                    />
-                    <Button fullWidth variant="contained" color="primary" type="submit"  sx={{ mt: 3, py: 1.5, fontWeight: 600 }}>
-                        Login
-                    </Button>
-                    <Typography
-                        variant="body2"
-                        align="right"
-                        sx={{ mt: 3, mb: 1, cursor: 'pointer', color: '#1976d2', '&:hover': { textDecoration: 'underline' } }}
-                        onClick={handleForgotPasswordClick}
-                    >
-                        Forgot Password?
-                    </Typography>
-                </form>
-            </Paper>
-        </Container>
+                            }}
+                        />
+                        <Button
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            type="submit"
+                            sx={{ mt: 3, py: 1.5, fontWeight: 600 }}
+                        >
+                            Login
+                        </Button>
+                        <Typography
+                            variant="body2"
+                            align="right"
+                            sx={{
+                            mt: 3,
+                            mb: 1,
+                            cursor: 'pointer',
+                            color: '#1976d2',
+                            '&:hover': { textDecoration: 'underline' },
+                            }}
+                            onClick={handleForgotPasswordClick}
+                        >
+                            Forgot Password?
+                        </Typography>
+                    </form>
+                </Paper>
+            </Container>
         </Box>
+    </Box>
     )
 }
 
